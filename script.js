6723591a1a48bdc63ce3356c22ca6fb784d912f0
AntonTyurin-1 <video888@inbox.ru>
2022-05-16 15:40:17 +0300

    последние изменения

---
+++ script.js
@@ -0,0 +1,13 @@
+const arrTasks = [
+	{ id: 0, text: 'Купить хлеб', complited: false },
+	{ id: 1, text: 'Купить масло', complited: false },
+ 
+]
+
+function createListItem() {
+	const input = document.createElement('input');
+	input.classList.add('list__input');
+
+
+}
+console.log(arrTasks)
