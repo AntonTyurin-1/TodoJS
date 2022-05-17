2c4aa6d1cddcea0147c857f6d28a596be8abf7e0
AntonTyurin-1 <video888@inbox.ru>
2022-05-17 19:08:07 +0300

    addTasts

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-const arrTasks = [
-	{ id: 0, text: 'Купить хлеб', complited: false },
-	{ id: 1, text: 'Купить масло', complited: false },
+const arrTasks = [		
 ]
-	
-function createListItem(renderTasks) {
+/* 	-----------CREATE---------------------------------------------------------- */
+
+function createListItem(task) {
    const li = document.createElement('li')
 	li.classList.add('list__item')
 
    const input = document.createElement('input')
-	input.type('checkbox')
+	input.type = 'checkbox'
 	input.classList.add('list__input')
 
    const p = document.createElement('p') 
 	p.classList.add('list__text')
-	/* p.innerHTML = newTask.text */
+	p.innerHTML = task.text
 	
 	const img = document.createElement('img')
 	img.src = 'img/del.svg'
@@ -21,18 +20,19 @@ function createListItem(renderTasks) {
 	let div =  document.createElement('div')
 	div.append(img)
 
-   li.prepend(input, p, div)
+   li.append(input, p, div)
 	const ul = document.querySelector('ul')
 	ul.prepend(li)
-	return li	
-}
+	return li 	
+} 
 
 
-function renderTasks() {
-	arrTasks.forEach(item => {
+function renderTasks(tasks) {
+	tasks.forEach(item => {
 		createListItem(item)
 	})			
 }
+renderTasks(arrTasks)
 
 
 
@@ -44,13 +44,21 @@ function addTasks() {
 	const newTask = { id: maxId, text: formInput.value, complited: false }
 
 	arrTasks.push(newTask)
-	
-	renderTasks()
+
+	renderTasks(arrTasks)
+	/* console.log(arrTasks) */
 }
 
 const formButton = document.querySelector('.form__btn')
-formButton.onclick = function() {
-	createListItem()
+formButton.addEventListener('click', addTasks)
+
+/* 	-----------DELETE----------------------------------------------------- */
+
+function delTask(id) {
+	const arrDel = arrTasks.filter(elem => elem.id != id)
+	renderTasks(arrDel)
 }
 
+const imgDel = document.querySelector('img')
+imgDel.addEventListener('click', delTask)
 
