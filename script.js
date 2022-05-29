4b4047f22bdbfbcf92c80800d16fbfce874da13e
AntonTyurin-1 <video888@inbox.ru>
2022-05-29 18:15:24 +0300

    Finish

--- script.js
+++ script.js
@@ -1,146 +1,116 @@
-let arrTasks = []
-	
-
+let arrTasks = [];
+ if(localStorage.getItem('task',arrTasks)) {
+   arrTasks = JSON.parse(localStorage.getItem('task',arrTasks))
+   renderTasks(arrTasks)
+ }
 /* 	-----------CREATE---------------------------------------------------------- */
 
 function createListItem(task) {
-   let li = document.createElement('li')
-	li.classList.add('list__item')
+  let li = document.createElement("li");
+  li.classList.add("list__item");
 
-   let input = document.createElement('input')
+  let input = document.createElement("input");
 
-	input.type = 'checkbox'
-	input.checked = task.complited
-	input.classList.add('list__input')
-	input.id = task.id
-	/* input.onclick = checkInput */
+  input.type = "checkbox";
+  input.checked = task.complited;
+  input.classList.add("list__input");
+  input.id = task.id;
 
-   let label = document.createElement('label') 
-	label.classList.add('list__label')
+  let label = document.createElement("label");
+  label.classList.add("list__label");
 
-	label.innerHTML = task.text
-	label.for = task.id
-	label.onclick = checkInput
-	
-	
-	let img = document.createElement('img')
-	img.src = 'img/del.svg'
-	img.alt = 'image'
+  label.innerHTML = task.text;
+  label.for = task.id;
+  label.onclick = checkInput;
 
-	let div = document.createElement('div')
-	div.append(img)
-	div.addEventListener('click', delTask)
+  let img = document.createElement("img");
+  img.src = "img/del.svg";
+  img.alt = "image";
 
-   li.append(input, label, div)
-	
-	li.id = task.id
-	
-	/*  console.log(task)  */
-	return li 	
-} 
+  let div = document.createElement("div");
+  div.append(img);
+  div.addEventListener("click", delTask);
+
+  li.append(input, label, div);
+
+  li.id = task.id;
+
+  return li;
+}
 
 /* 	-----------RENDER----------------------------------------------------- */
 
 function renderTasks(tasks) {
-	const ul = document.querySelector('ul')
-	ul.innerHTML = ''
-	tasks.forEach(item => {
-		let li = createListItem(item)
-		ul.prepend(li)
-	})
+  const ul = document.querySelector("ul");
+  ul.innerHTML = "";
+  tasks.forEach((item) => {
+    let li = createListItem(item);
+    ul.prepend(li);
+  });
 }
-renderTasks(arrTasks)
+renderTasks(arrTasks);
 
 /* 	-----------ADD----------------------------------------------------- */
 
 function addTasks() {
-	const formInput = document.querySelector('.form__input')
+  const formInput = document.querySelector(".form__input")
 
-	const maxId = + new Date()
+  const maxId = +new Date()
 
-	const newTask = { id: maxId, text: formInput.value, complited: false }
+  const newTask = { id: maxId, text: formInput.value, complited: false }
 
-	arrTasks.push(newTask)
+  arrTasks.push(newTask)
 
-	renderTasks(arrTasks)
+  renderTasks(arrTasks)
 
-/* 	localStorage.setItem(formInput.value, JSON.stringify(arrTasks))
-	function delStorege() {
-		if (delTask(item)) {
-			localStorage.removeItem(formInput.value)
-		}
-		let newT = localStorage.getItem(arrTasks)
-		let localS = JSON.parse(newT)
-	} */
+  formInput.value = ''
 
+  localStorage.setItem('task', JSON.stringify(arrTasks))
 }
-
-const formButton = document.querySelector('.form__btn')
-formButton.addEventListener('click', addTasks)
-
-const formInput = document.querySelector('.form__input')
-formInput.addEventListener('keydown', function(event) {
-   if(event.code === 'Enter') {
-		addTasks()
-	} 
-})
-
 
 /* 	-----------CHECKED----------------------------------------------------- */
 
 function checkInput(event) {
-	let id = event.target.parentNode.id
-	 arrTasks.map(elem => {
-		if (elem.id == id) {
-		 elem.complited = !elem.complited
-		}
-	})
-	renderTasks(arrTasks)
+  let id = event.target.parentNode.id;
+  arrTasks.map((elem) => {
+    if (elem.id == id) {
+      elem.complited = !elem.complited;
+    }
+  })
+  localStorage.setItem('task', JSON.stringify(arrTasks))
+  renderTasks(arrTasks);
 }
 
 /* 	-----------DELETE----------------------------------------------------- */
 
 function delTask(event) {
-	let id = event.target.parentNode.parentNode.id
-	let arrDel = arrTasks.filter(elem => elem.id != id)
-	arrTasks = arrDel
-	renderTasks(arrTasks)
+  localStorage.getItem('task', JSON.stringify(arrTasks))
+  let id = event.target.parentNode.parentNode.id
+  let arrDel = arrTasks.filter((elem) => elem.id != id)
+  arrTasks = arrDel
+  localStorage.setItem('task', JSON.stringify(arrTasks))
+  renderTasks(arrTasks) 
 }
 
 /* 	-----------ALL----------------------------------------------------- */
 
 function allTask() {
-	let all = arrTasks
-	renderTasks(all)
+  renderTasks(arrTasks);
 }
-
- let btn1 = document.querySelector('.btn1')
-btn1.onclick = allTask 
-
 
 /* 	-----------checkedTaskL----------------------------------------------------- */
 
-
 function checkedTask(item) {
-	let check = arrTasks.filter(item => item.complited == true)
+  let check = arrTasks.filter((item) => item.complited == true);
 
-	 renderTasks(check)
-
+  renderTasks(check);
 }
-let btn2 = document.querySelector('.btn2')
-btn2.onclick = checkedTask
-
 
 /* 	-----------NotCheckedTask----------------------------------------------------- */
 
 function NotCheckedTask(item) {
-	let notCheck = arrTasks.filter(item => item.complited !== true)
+  let notCheck = arrTasks.filter((item) => item.complited !== true)
 
-	renderTasks(notCheck)
-}
-let btn3 = document.querySelector('.btn3')
-btn3.onclick = NotCheckedTask
-
-
-
-
+  renderTasks(notCheck) 
+ }
+ /* 	---------------------------------------------------------------- */
